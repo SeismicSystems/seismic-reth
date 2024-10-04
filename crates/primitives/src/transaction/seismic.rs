4c41f7c7318c667b056b62f17ae2ab443a38f5c5
Spencer Solit <sol@seismic.systems>
2024-10-04 16:24:17 -0400

    encryption example

--- crates/primitives/src/transaction/seismic.rs
+++ crates/primitives/src/transaction/seismic.rs
@@ -10,3 +10,22 @@ pub struct SeismicTransactionSigned {
     // pub transaction: TxType, // This should include the seismic tranaction type
     pub nonce: u64,
 }
+
+
+
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, OsRng as AesRng},
+    Aes256Gcm, Key
+};
+
+pub fn aes_gcm_example() {
+    let rng = AesRng::default();
+    let key: Key<Aes256Gcm> = Aes256Gcm::generate_key(rng);
+
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Aes256Gcm::generate_nonce(rng);
+    let ciphertext = cipher.encrypt(&nonce, b"plaintext message".as_ref()).unwrap();
+    let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).unwrap();
+    assert_eq!(&plaintext, b"plaintext message");
+    println!("AES-GCM Encrypted successfully: {:?}", ciphertext);
+}
