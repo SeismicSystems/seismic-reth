a8a017ba00d6e7a7f3b31e7ff9c8298777c581bd
Ameya <f20200139@pilani.bits-pilani.ac.in>
2024-10-10 22:10:11 +0530

    automated tests to run seismic-reth silently through script

--- testing/seismic-tests/seismicTransaction.test.ts
+++ testing/seismic-tests/seismicTransaction.test.ts
@@ -1,7 +1,8 @@
-import { describe, test, expect } from "bun:test"
+import { describe, test, expect, afterAll } from "bun:test"
 import { createPublicClient, http, createWalletClient, defineChain} from "viem"
 import { privateKeyToAccount } from "viem/accounts"
-
+import { runSeismicReth } from "./seismicReth"
+import { killProcess } from "./process"
 const TEST_ADDRESS = '0x5615deb798bb3e4dfa0139dfa1b3d433cc23b72f'
 const TEST_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
 
@@ -19,13 +20,17 @@ const seismicDevnet = defineChain({
   rpcUrls: {
     default: {
       http: ['http://localhost:8545'],
-    //   http: ['https://seismic-devnet.com/rpc'],
-    //   // webSocket: ['https://seismic-devnet.com/ws']
     },
   }
 })
 
-const publicClient = createPublicClient({ chain: seismicDevnet, transport: http() })
+const seismicRethProcess = await runSeismicReth({ silent: true })
+
+const exitProcess = async (code: 0 | 1) => {
+  await killProcess(seismicRethProcess.process);
+  process.exit(code)
+}
+
 
 const walletClient = createWalletClient({
   account,
@@ -34,7 +39,7 @@ const walletClient = createWalletClient({
 })
 
 describe('Seismic Transaction', () => {
-  test('node detects seismic transaction', async () => {
+  test('node detects and parses seismic transaction', async () => {
     const response = await walletClient.request({
       // @ts-ignore
       method: "seismic_sendTransaction",
@@ -59,8 +64,11 @@ describe('Seismic Transaction', () => {
       ]
     });
 
-    // Add assertions here to verify the response
     expect(response).toBeDefined();
     console.log('Seismic transaction response:', response);
   });
 });
+
+afterAll(async () => {
+  await exitProcess(0)
+})
