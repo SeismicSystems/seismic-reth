8d06f90aeb4dc1c6c1f66a7ba42e89b7f98a16c7
phexyz <32248504+phexyz@users.noreply.github.com>
2024-10-23 14:13:52 -0400

    finished block production

--- crates/ethereum/node/tests/e2e/p2p.rs
+++ crates/ethereum/node/tests/e2e/p2p.rs
@@ -1,5 +1,5 @@
 use crate::utils::eth_payload_attributes;
-use reth_chainspec::{ChainSpecBuilder, DEV};
+use reth_chainspec::{ChainSpecBuilder, MAINNET};
 use reth_e2e_test_utils::{setup, transaction::{SeismicTransactionTestContext, TransactionTestContext}};
 use reth_node_ethereum::EthereumNode;
 use std::sync::Arc;
@@ -12,7 +12,7 @@ async fn can_sync() -> eyre::Result<()> {
         2,
         Arc::new(
             ChainSpecBuilder::default()
-                .chain(DEV.chain)
+                .chain(MAINNET.chain)
                 .genesis(serde_json::from_str(include_str!("../assets/genesis.json")).unwrap())
                 .cancun_activated()
                 .build(),
@@ -24,30 +24,8 @@ async fn can_sync() -> eyre::Result<()> {
     let mut second_node = nodes.pop().unwrap();
     let mut first_node = nodes.pop().unwrap();
 
-    // // first block with regular transfer transaction
-    // let raw_tx = TransactionTestContext::transfer_tx_bytes(1, wallet.inner.clone()).await;
-
-    // // Make the first node advance
-    // let tx_hash = first_node.rpc.inject_tx(raw_tx).await?;
-
-    // // make the node advance
-    // let (payload, _) = first_node.advance_block(vec![], eth_payload_attributes).await?;
-
-    // let block_hash = payload.block().hash();
-    // let block_number = payload.block().number;
-
-    // // assert the block has been committed to the blockchain
-    // first_node.assert_new_block(tx_hash, block_hash, block_number).await?;
-
-    // // only send forkchoice update to second node
-    // second_node.engine_api.update_forkchoice(block_hash, block_hash).await?;
-
-    // // expect second node advanced via p2p gossip
-    // second_node.assert_new_block(tx_hash, block_hash, 1).await?;
-
-    // second block for encrypted transaction
-
-    let raw_tx = SeismicTransactionTestContext::deploy_tx_bytes(DEV.chain.id(), wallet.inner, 0).await;
+    // ==================== first block with regular transfer transaction ========
+    let raw_tx = TransactionTestContext::transfer_tx_bytes(MAINNET.chain.id(), wallet.inner.clone()).await;
 
     // Make the first node advance
     let tx_hash = first_node.rpc.inject_tx(raw_tx).await?;
@@ -66,6 +44,27 @@ async fn can_sync() -> eyre::Result<()> {
 
     // expect second node advanced via p2p gossip
     second_node.assert_new_block(tx_hash, block_hash, 1).await?;
+
+    // ==================== second block for encrypted transaction ====================
+    let raw_tx = SeismicTransactionTestContext::deploy_tx_bytes(MAINNET.chain.id(), wallet.inner, 1).await;
+
+    // Make the first node advance
+    let tx_hash = first_node.rpc.inject_tx(raw_tx).await?;
+
+    // make the node advance
+    let (payload, _) = first_node.advance_block(vec![], eth_payload_attributes).await?;
+
+    let block_hash = payload.block().hash();
+    let block_number = payload.block().number;
+
+    // assert the block has been committed to the blockchain
+    first_node.assert_new_block(tx_hash, block_hash, block_number).await?;
+
+    // only send forkchoice update to second node
+    second_node.engine_api.update_forkchoice(block_hash, block_hash).await?;
+
+    // expect second node advanced via p2p gossip
+    second_node.assert_new_block(tx_hash, block_hash, 2).await?;
 
     Ok(())
 }
