b08109da7463e9db4be7140b3ebaea6dbcf0d755
phexyz <32248504+phexyz@users.noreply.github.com>
2024-10-23 16:05:45 -0400

    format

--- crates/transaction-pool/src/validate/eth.rs
+++ crates/transaction-pool/src/validate/eth.rs
@@ -13,7 +13,7 @@ use reth_chainspec::{ChainSpec, EthereumHardforks};
 use reth_primitives::{
     constants::eip4844::MAX_BLOBS_PER_BLOCK, GotExpected, InvalidTransactionError, SealedBlock,
     EIP1559_TX_TYPE_ID, EIP2930_TX_TYPE_ID, EIP4844_TX_TYPE_ID, EIP7702_TX_TYPE_ID,
-    LEGACY_TX_TYPE_ID, SEISMIC_TX_TYPE_ID
+    LEGACY_TX_TYPE_ID, SEISMIC_TX_TYPE_ID,
 };
 use reth_storage_api::{AccountReader, BlockReaderIdExt, StateProviderFactory};
 use reth_tasks::TaskSpawner;
