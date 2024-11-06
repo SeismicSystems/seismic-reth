6366244f524fc44632690971cef7b597279d0d90
Spencer Solit <sol@seismic.systems>
2024-11-06 12:58:56 -0500

    removed unneeded client.rs file

--- crates/tee/src/client.rs
+++
@@ -1,8 +0,0 @@
-//! This module provides functionality for encryption and decryption
-//! using a Trusted Execution Environment (TEE) client.
-//!
-//! The TEE client makes HTTP requests to a TEE server to perform
-//! encryption and decryption operations. The main structures and
-//! traits define the API and implementation for the TEE client.
-
-
