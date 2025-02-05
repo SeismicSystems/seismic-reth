4829309fe5e2818109832cb909cbeb8423997e28
SFYLL <sd@seismic.systems>
2025-02-05 14:27:53 +0100

    rm unused imports

--- crates/seismic/node/tests/integration.rs
+++ crates/seismic/node/tests/integration.rs
@@ -4,7 +4,7 @@ use alloy_primitives::{
     aliases::{B96, U96},
     hex,
     hex::FromHex,
-    Address, Bytes, IntoLogData, TxKind, B256, U256,
+     Bytes, IntoLogData, TxKind, B256, U256,
 };
 use alloy_provider::{create_seismic_provider, test_utils, Provider, SendableTx};
 use alloy_rpc_types::{
@@ -19,9 +19,7 @@ use seismic_node::utils::test_utils::{
     client_decrypt, get_nonce, get_signed_seismic_tx_bytes, get_signed_seismic_tx_typed_data,
     get_unsigned_seismic_tx_request,
 };
-use seismic_rpc_api::rpc::EthApiOverrideClient;
-use serde_json::{json, Value};
-use std::{path::PathBuf, str::FromStr, thread, time::Duration};
+use std::{path::PathBuf, thread, time::Duration};
 use sysinfo::{Pid, PidExt, ProcessExt, System, SystemExt};
 use tee_service_api::aes_decrypt;
 use tokio::process::Child;
