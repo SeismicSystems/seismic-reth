7ea300f9b02ae5cc8fef1a6b713e99bdcb2df14b
Spencer Solit <sol@seismic.systems>
2025-05-16 15:18:57 -0400

    wire in revm_spec fn

--- crates/seismic/evm/src/config.rs
+++ crates/seismic/evm/src/config.rs
@@ -6,19 +6,15 @@ use reth_seismic_chainspec::{SeismicChainSpec};
 
 /// Map the latest active hardfork at the given header to a revm [`SeismicSpecId`].
 pub fn revm_spec(chain_spec: impl SeismicHardfork, header: impl BlockHeader) -> OpSpecId {
-    revm_spec_by_timestamp_after_bedrock(chain_spec, header.timestamp())
+    revm_spec_by_timestamp_seismic(&chain_spec, header.timestamp())
 }
 
 /// Map the latest active hardfork at the given timestamp or block number to a revm [`SeismicSpecId`].
-pub fn revm_spec_by_timestamp_and_block_number(
+/// 
+/// For now our only hardfork is MERCURY, so we only return MERCURY
+fn revm_spec_by_timestamp_seismic(
     chain_spec: &SeismicChainSpec,
     timestamp: u64,
-    block_number: u64,
-) -> SpecId {
-    if chain_spec
-        .fork(SeismicHardfork::MERCURY)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
+) -> SeismicSpecId {
         SeismicSpecId::MERCURY
-    } 
 }
--- crates/seismic/evm/src/lib.rs
+++ crates/seismic/evm/src/lib.rs
@@ -36,6 +36,9 @@ mod receipts;
 pub use receipts::*;
 mod build;
 
+mod config;
+use config::revm_spec;
+
 pub use alloy_seismic_evm::{SeismicEvm, SeismicEvmFactory};
 
 /// Ethereum-related EVM configuration.
@@ -133,10 +136,10 @@ impl ConfigureEvm for SeismicEvmConfig {
         parent: &Header,
         attributes: &NextBlockEnvAttributes,
     ) -> Result<EvmEnv<SeismicSpecId>, Self::Error> {
-        // ensure we're not missing any timestamp based hardforks
-        // let spec_id = revm_spec_by_timestamp_after_bedrock(self.chain_spec(),
-        // attributes.timestamp);
-        let spec_id = SeismicSpecId::MERCURY; // this is the only SeismicSpecId for now
+        let spec_id = revm_spec(
+            self.chain_spec(),
+            attributes.timestamp,
+        );
 
         // configure evm env based on parent block
         let cfg = CfgEnv::new().with_chain_id(self.chain_spec().chain().id()).with_spec(spec_id);
@@ -210,88 +213,6 @@ impl ConfigureEvm for SeismicEvmConfig {
             ommers: &[],
             withdrawals: attributes.withdrawals.map(Cow::Owned),
         }
-    }
-}
-
-/// Code copied and pasted from reth-evm-ethereum
-/// Map the latest active hardfork at the given timestamp or block number to a revm [`SpecId`].
-pub fn revm_spec_by_timestamp_and_block_number(
-    chain_spec: &ChainSpec,
-    timestamp: u64,
-    block_number: u64,
-) -> SpecId {
-    if chain_spec
-        .fork(EthereumHardfork::Osaka)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::OSAKA
-    } else if chain_spec
-        .fork(EthereumHardfork::Prague)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::PRAGUE
-    } else if chain_spec
-        .fork(EthereumHardfork::Cancun)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::CANCUN
-    } else if chain_spec
-        .fork(EthereumHardfork::Shanghai)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::SHANGHAI
-    } else if chain_spec.is_paris_active_at_block(block_number) {
-        SpecId::MERGE
-    } else if chain_spec
-        .fork(EthereumHardfork::London)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::LONDON
-    } else if chain_spec
-        .fork(EthereumHardfork::Berlin)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::BERLIN
-    } else if chain_spec
-        .fork(EthereumHardfork::Istanbul)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::ISTANBUL
-    } else if chain_spec
-        .fork(EthereumHardfork::Petersburg)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::PETERSBURG
-    } else if chain_spec
-        .fork(EthereumHardfork::Byzantium)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::BYZANTIUM
-    } else if chain_spec
-        .fork(EthereumHardfork::SpuriousDragon)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::SPURIOUS_DRAGON
-    } else if chain_spec
-        .fork(EthereumHardfork::Tangerine)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::TANGERINE
-    } else if chain_spec
-        .fork(EthereumHardfork::Homestead)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::HOMESTEAD
-    } else if chain_spec
-        .fork(EthereumHardfork::Frontier)
-        .active_at_timestamp_or_number(timestamp, block_number)
-    {
-        SpecId::FRONTIER
-    } else {
-        panic!(
-            "invalid hardfork chainspec: expected at least one hardfork, got {:?}",
-            chain_spec.hardforks
-        )
     }
 }
 
