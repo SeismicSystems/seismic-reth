f4bf2a961c8fe80be81db42a8bfb2b63276ef0c1
Spencer Solit <sol@seismic.systems>
2025-06-11 14:36:03 -0400

    make trie crate build

--- crates/trie/trie/benches/hash_post_state.rs
+++ crates/trie/trie/benches/hash_post_state.rs
@@ -3,7 +3,7 @@ use alloy_primitives::{keccak256, map::HashMap, Address, B256, U256};
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use proptest::{prelude::*, strategy::ValueTree, test_runner::TestRunner};
 use reth_trie::{HashedPostState, HashedStorage, KeccakKeyHasher};
-use revm::state::FlaggedStorage;
+use revm_state::FlaggedStorage;
 use revm_database::{states::BundleBuilder, BundleAccount};
 
 pub fn hash_post_state(c: &mut Criterion) {
