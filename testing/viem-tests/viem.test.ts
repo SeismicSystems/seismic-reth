f3e308bf04dd076d1c4d19d8e916c9873b722373
cdrappi <c@seismic.systems>
2025-07-03 16:55:37 -0400

    more debugging

--- testing/viem-tests/viem.test.ts
+++ testing/viem-tests/viem.test.ts
@@ -1,7 +1,13 @@
-import type { Chain } from "viem";
-import { localSeismicDevnet } from "seismic-viem";
+import { encodeFunctionData, http, parseEther, stringToBytes, stringToHex, type Chain } from "viem";
+import {
+    AesGcmCrypto,
+    createShieldedWalletClient,
+    localSeismicDevnet,
+    randomEncryptionNonce,
+    signSeismicTxTypedData,
+} from "seismic-viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { beforeAll, afterAll, describe, test } from "bun:test";
+import { beforeAll, afterAll, describe, test, expect } from "bun:test";
 import {
     setupNode,
     testAesKeygen,
@@ -34,12 +40,14 @@ const encryptionSk =
 const encryptionPubkey =
     "0x028e76821eb4d77fd30223ca971c49738eb5b5b71eabe93f96b348fdce788ae5a0";
 
-const HOST = 'node-4.seismicdev.net'
+const HOST = "node-4.seismicdev.net";
 
-let url: string = `https://${HOST}/rpc`
+let url: string = `https://${HOST}/rpc`;
 let wsUrl: string = `wss://${HOST}/ws`;
-let exitProcess: () => Promise<void> = async () => { process.exit(0) }
-let pcParams: { chain: Chain; url: string }
+let exitProcess: () => Promise<void> = async () => {
+    process.exit(0);
+};
+let pcParams: { chain: Chain; url: string };
 
 beforeAll(async () => {
     /*
@@ -56,6 +64,90 @@ beforeAll(async () => {
     wsUrl = `ws://localhost:${port}`;
     */
 });
+
+describe("Moonhatch seismic tx", async () => {
+    const client = await createShieldedWalletClient({
+        chain,
+        account,
+        transport: http(url),
+        encryptionSk,
+    });
+
+    const plaintext = encodeFunctionData({
+        abi: [
+            {
+                type: "function",
+                name: "buy",
+                inputs: [
+                    {
+                        name: "coinId",
+                        type: "uint32",
+                        internalType: "uint32",
+                    },
+                ],
+                outputs: [
+                    {
+                        name: "weiRefunded",
+                        type: "uint256",
+                        internalType: "uint256",
+                    },
+                ],
+                stateMutability: "payable",
+            },
+        ],
+        functionName: "buy",
+        args: [56249],
+    });
+    console.log(await client.getTeePublicKey())
+    const aesKey = client.getEncryption()
+    console.log(`AES Key: ${aesKey}`)
+    const aesCipher = new AesGcmCrypto(aesKey)
+    const encryptionNonce = "0x7da3a99bf0f90d56551d99ea"
+    const encrypted = await aesCipher.encrypt(plaintext, encryptionNonce)
+    const nonce = await client.getTransactionCount({ address: account.address })
+
+    console.log(encrypted)
+    console.log(encryptionNonce)
+    console.log(plaintext);
+    const { typedData, signature } = await signSeismicTxTypedData(client, {
+        chainId: client.chain.id,
+        nonce,
+        gasPrice: 360000n,
+        gas: 169477n,
+        to: "0x3aB946eEC2553114040dE82D2e18798a51cf1e14",
+        value: parseEther('0.1'),
+        data: encrypted,
+        type: "seismic",
+        encryptionPubkey: encryptionPubkey,
+        encryptionNonce: encryptionNonce,
+        messageVersion: 2,
+    })
+    console.log(typedData)
+    console.log(signature)
+
+    // // @ts-ignore
+    // const hash = await client.sendRawTransaction({serializedTransaction: { data: typedData, signature }})
+    // console.log(hash)
+    // const receipt = await client.waitForTransactionReceipt({ hash })
+    // console.log(receipt)
+});
+
+describe("DECRYPT", async () => {
+    // const client = await createShieldedWalletClient({
+    //     chain,
+    //     account,
+    //     transport: http(url),
+    // });
+    // const aesKey = client.getEncryption()
+    // console.log(`AES Key: ${aesKey}`)
+    // const plaintext = stringToHex("Hello, world!")
+    // console.log(`Plaintext: ${plaintext}`)
+    // const aesCipher = new AesGcmCrypto(aesKey)
+    // const encryptionNonce = "0x7da3a99bf0f90d56551d99ea"
+    // const encrypted = await aesCipher.encrypt(plaintext, encryptionNonce)
+
+    // expect(encrypted).toBe("0x62a9351e3ad631c807e89586341d4a9ea3e64188aa2b65cd000140ea15")
+})
 
 describe("Seismic Contract", async () => {
     test(
