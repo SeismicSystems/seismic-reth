c0911cf4fbe642667df6581829d32ddf043cbd59
Spencer Solit <sol@seismic.systems>
2025-07-14 15:46:25 -0400

    add upgrade contract to mainnet genesis

--- crates/seismic/chainspec/src/lib.rs
+++ crates/seismic/chainspec/src/lib.rs
@@ -20,7 +20,7 @@ use reth_seismic_forks::{SEISMIC_DEV_HARDFORKS, SEISMIC_MAINNET_HARDFORKS};
 /// Genesis hash for the Seismic mainnet
 /// Calculated by rlp encoding the genesis header and hashing it
 pub const SEISMIC_MAINNET_GENESIS_HASH: B256 =
-    b256!("0xee01857dd54ff6d7de6a90b2a76b42a86b7ea8f3a6d2ae27bd45ee6b3698b7b2");
+    b256!("0xd548d4a126d72e43d893b3826c07ad24bddbaeee267489baff2f73fff2ac0976");
 
 /// Genesis hash for the Seismic devnet
 /// Calculated by rlp encoding the genesis header and hashing it
@@ -139,9 +139,9 @@ mod tests {
 
     #[test]
     fn genesis_header_hash() {
-        // Confirm how mainnet genesis header hash is calculated
+        // Confirm how eth mainnet genesis header hash is calculated
         let expected = MAINNET_GENESIS_HASH;
-        let genesis = serde_json::from_str(include_str!("../res/genesis/mainnet.json"))
+        let genesis = serde_json::from_str(include_str!("../../../chainspec/res/genesis/mainnet.json"))
             .expect("Can't deserialize Mainnet genesis json");
         let hardforks = EthereumHardfork::mainnet().into();
         let genesis_header = make_genesis_header(&genesis, &hardforks);
