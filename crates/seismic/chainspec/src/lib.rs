26ae97804d66d3518bd4915234490ab1d79c5973
Spencer Solit <sol@seismic.systems>
2025-07-14 15:37:09 -0400

    update devnet genesis hash const

--- crates/seismic/chainspec/src/lib.rs
+++ crates/seismic/chainspec/src/lib.rs
@@ -26,7 +26,7 @@ pub const SEISMIC_MAINNET_GENESIS_HASH: B256 =
 /// Calculated by rlp encoding the genesis header and hashing it
 /// Currently matches the mainnet genesis hash because they have matching hardforks
 pub const SEISMIC_DEV_GENESIS_HASH: B256 =
-    b256!("0x683713729fcb72be6f3d8b88c8cda3e10569d73b9640d3bf6f5184d94bd97616");
+    b256!("0x655cc1ecc77fe1eab4b1e62a1f461b7fddc9b06109b5ab3e9dc68c144b30c773");
 
 /// Seismic devnet specification
 pub static SEISMIC_DEV: LazyLock<Arc<ChainSpec>> = LazyLock::new(|| {
