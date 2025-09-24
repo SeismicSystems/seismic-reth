842a5bfa59e4418d422cd6ee701807aed76473d8
cdrappi <c@seismic.systems>
2025-09-24 11:14:39 -0400

    use rpc converter

--- crates/seismic/rpc/src/eth/mod.rs
+++ crates/seismic/rpc/src/eth/mod.rs
@@ -281,20 +281,9 @@ where
     type EthApi = SeismicEthApi<N, SeismicRpcConvert<N, NetworkT>>;
 
     async fn build_eth_api(self, ctx: EthApiCtx<'_, N>) -> eyre::Result<Self::EthApi> {
-        let eth_api = reth_rpc::EthApiBuilder::new(
-            ctx.components.provider().clone(),
-            ctx.components.pool().clone(),
-            ctx.components.network().clone(),
-            ctx.components.evm_config().clone(),
-        )
-        .eth_cache(ctx.cache)
-        .task_spawner(ctx.components.task_executor().clone())
-        .gas_cap(ctx.config.rpc_gas_cap.into())
-        .max_simulate_blocks(ctx.config.rpc_max_simulate_blocks)
-        .eth_proof_window(ctx.config.eth_proof_window)
-        .fee_history_cache_config(ctx.config.fee_history_cache)
-        .proof_permits(ctx.config.proof_permits)
-        .build_inner();
+        let rpc_converter = RpcConverter::new(());
+
+        let eth_api = ctx.eth_api_builder().with_rpc_converter(rpc_converter).build_inner();
 
         Ok(SeismicEthApi { inner: Arc::new(eth_api) })
     }
