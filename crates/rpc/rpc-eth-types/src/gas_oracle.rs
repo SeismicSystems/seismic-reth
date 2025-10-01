7fca5a5e370a4c84d0ad9667bf68d544c9cf14d0
cdrappi <c@seismic.systems>
2025-10-01 16:58:26 -0400

    diff

--- crates/rpc/rpc-eth-types/src/gas_oracle.rs
+++ crates/rpc/rpc-eth-types/src/gas_oracle.rs
@@ -149,7 +149,7 @@ where
 
         // if we have stored a last price, then we check whether or not it was for the same head
         if inner.last_price.block_hash == header.hash() {
-            return Ok(inner.last_price.price);
+            return Ok(inner.last_price.price)
         }
 
         // if all responses are empty, then we can return a maximum of 2*check_block blocks' worth
@@ -311,7 +311,7 @@ where
 
         // if we have stored a last price, then we check whether or not it was for the same head
         if inner.last_price.block_hash == header.hash() {
-            return Ok(inner.last_price.price)
+            return Ok(inner.last_price.price);
         }
 
         let mut suggestion = min_suggested_priority_fee;
