a9503102dd239d5d1c041f9ab10c80e3b68408a4
Ameya Deshmukh <ad@seismic.systems>
2025-11-03 20:34:23 +0530

    rm comments and println

--- crates/seismic/primitives/src/transaction/signed.rs
+++ crates/seismic/primitives/src/transaction/signed.rs
@@ -817,11 +817,6 @@ mod tests {
 
         #[test]
         fn test_roundtrip_compact_encode_envelope(reth_tx in arb::<SeismicTransactionSigned>()) {
-            println!("{}", reth_tx.transaction().tx_type());
-            // if reth_tx.transaction().tx_type() == SeismicTxType::Eip4844 {
-            //     // TODO: make this work for eip4844 in seismic-alloy
-            //     return Ok(())
-            // }
             let mut expected_buf = Vec::<u8>::new();
             let expected_len = reth_tx.to_compact(&mut expected_buf);
 
@@ -835,12 +830,6 @@ mod tests {
 
         #[test]
         fn test_roundtrip_compact_decode_envelope(reth_tx in arb::<SeismicTransactionSigned>()) {
-            println!("{}", reth_tx.transaction().tx_type());
-            // if reth_tx.transaction().tx_type() == SeismicTxType::Eip4844 {
-            //     // TODO: make this work for eip4844 in seismic-alloy
-            //     return Ok(())
-            // }
-
             let mut buf = Vec::<u8>::new();
             let len = reth_tx.to_compact(&mut buf);
 
