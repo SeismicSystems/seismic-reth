7b8fe3607c61ef3bb78cb812913446e1fd07f731
Ameya Deshmukh <ad@seismic.systems>
2026-01-10 03:12:53 +0530

    better

--- crates/seismic/evm/src/config.rs
+++ crates/seismic/evm/src/config.rs
@@ -7,14 +7,14 @@ use seismic_revm::SeismicSpecId;
 
 /// Map the latest active hardfork at the given header to a revm [`SeismicSpecId`].
 pub fn revm_spec(chain_spec: &SeismicChainSpec, header: &Header) -> SeismicSpecId {
-    revm_spec_by_timestamp_seismic(&chain_spec, header.timestamp())
+    revm_spec_by_timestamp_seismic(chain_spec, header.timestamp())
 }
 
 /// Map the latest active hardfork at the given timestamp or block number to a revm
 /// [`SeismicSpecId`].
 ///
 /// For now our only hardfork is MERCURY, so we only return MERCURY
-fn revm_spec_by_timestamp_seismic(
+const fn revm_spec_by_timestamp_seismic(
     _chain_spec: &SeismicChainSpec,
     _timestamp: u64,
 ) -> SeismicSpecId {
