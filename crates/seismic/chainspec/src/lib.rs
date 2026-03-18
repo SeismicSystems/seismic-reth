ab423ac26ed64530fea191927324a90d813b2e67
Matthias Wright <matthias.s.wright@gmail.com>
2026-03-18 20:21:11 +0800

    chore: update genesis

--- crates/seismic/chainspec/src/lib.rs
+++ crates/seismic/chainspec/src/lib.rs
@@ -28,7 +28,7 @@ pub const SEISMIC_MAINNET_GENESIS_HASH: B256 =
 /// Calculated by rlp encoding the genesis header and hashing it
 /// Currently matches the mainnet genesis hash because they have matching hardforks
 pub const SEISMIC_DEV_GENESIS_HASH: B256 =
-    b256!("0x3aade273783123091d493c481baa1e9e9bcde4b9cb87b29ae5398d83b88af441");
+    b256!("0x0894d7c689ac563109c35d8c036ced2b27d7c78365b092091eaa4f8eb75c98ad");
 
 /// Seismic devnet specification
 ///
