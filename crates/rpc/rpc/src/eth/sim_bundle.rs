7cd267d7cd6d6d28ebe3ac6ea1bf942510b235a9
Matthias Wright <matthias.s.wright@gmail.com>
2026-07-28 16:14:36 +0800

    feat: reject block overwrites

--- crates/rpc/rpc/src/eth/sim_bundle.rs
+++ crates/rpc/rpc/src/eth/sim_bundle.rs
@@ -247,7 +247,8 @@ where
                 let mut db = CacheDB::new(StateProviderDatabase::new(state));
 
                 // apply overrides
-                apply_block_overrides(block_overrides, &mut db, &mut evm_env.block_env);
+                apply_block_overrides(block_overrides, &mut db, &mut evm_env.block_env)
+                    .map_err(EthApiError::from_overrides_err)?;
 
                 let initial_coinbase_balance = DatabaseRef::basic_ref(&db, coinbase)
                     .map_err(EthApiError::from_eth_err)?
